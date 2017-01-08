34683d3d3d36a77ac4fa112525362d28ed2e32c0
Cyril Pecoraro <cyril.pecoraro@gmail.com>
2017-01-08 20:49:10 +0100

    fix code

--- bandsInTownHelper.py
+++ bandsInTownHelper.py
@@ -139,4 +139,6 @@ def concatenateDataBandsInTown():
     df.drop_duplicates(inplace=True)
 
     # Save Again
+    pd.DataFrame(df, columns=list(df.columns)).to_csv('total_bands_in_town.csv', index=False, encoding="utf-8")
+    print('file saved')
     return df
